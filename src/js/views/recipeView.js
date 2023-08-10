c32c1875703699efc0ff0723a01750a652612b0d
Maksym Netreba <51268714+NeMaksym@users.noreply.github.com>
2023-08-10 16:01:29 +0300

    Migrate to "Vite"

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js'
 import * as model from '../model.js'
 
-import icons from 'url:../../img/icons.svg'
+import icons from '../../img/icons.svg'
 import { Fraction } from 'fractional'
 
 class RecipeView extends View {

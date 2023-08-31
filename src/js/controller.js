87b0363a80ffcb98ab2f43297632b5f2392f2462
katrinmelody <77362916+katrinmelody@users.noreply.github.com>
2023-08-31 18:31:10 +0300

    Edit redirect uri

--- src/js/controller.js
+++ src/js/controller.js
@@ -21,7 +21,7 @@ const controlAuthentication = async () => {
         clientId: 'Ks6wUNCpcoNJzejw9JC5yNupXMK183Qm',
         authorizationParams: {
             audience: 'https://forkify-fvelk.ondigitalocean.app/api/v1',
-            redirect_uri: window.location.origin,
+            redirect_uri: window.location.href,
         },
         /**
          * Fixes: User is not logged in after page refresh
